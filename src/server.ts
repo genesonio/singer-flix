89e1d445e8eb8cba96ce665c2cc49983079d64eb
Genesio S Pacehco <genesio.s.p.28@gmail.com>
2022-07-13 16:08:15 -0300

    Setting AdminJS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import { adminJS, adminJSRouter } from './adminjs'
 import { sequelize } from './database'
 
 const app = express()
+
+// app.use(caminho, rotas)
+app.use(express.static('public'))
+
+app.use(adminJS.options.rootPath, adminJSRouter)
 
 const PORT = process.env.PORT || 3000
 
@@ -9,6 +15,6 @@ app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
     console.log('DB connection successful')
   })
-  
+
   console.log(`Server started successfully at port ${PORT}`)
 })
